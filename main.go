ababa6dc031cbdb71776f629d314f612dee9b39e
lovestaco <athreyac4@gmail.com>
2025-08-03 15:38:36 +0530

    ignore

--- main.go
+++ main.go
@@ -6,20 +6,22 @@ import (
 	"github.com/awnumar/memguard"
 )
 
-var protectedData *memguard.LockedBuffer
+var guardedData *memguard.LockedBuffer
 
-
-func dataHandler(w http.ResponseWriter, r *http.Request) {
-	dataCopy := memguard.NewBuffer(protectedData.Size())
-	copy(dataCopy.Buffer.Data(), protectedData.Buffer.Data())
-	defer dataCopy.Destroy()
-
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Write(dataCopy.Buffer.Data())
+func getData() *memguard.LockedBuffer {
+	return guardedData
 }
 
-func getData() string {
-	// Simulate reading sensitive data (could be from env or secure file)
+func dataHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	dataBuffer := getData()
+	w.Write(dataBuffer.Bytes())
+}
+
+func main() {
+	memguard.CatchInterrupt()
+	defer memguard.Purge()
+
 	rawData := `
  # Role and Context  
 You are an expert OpenAPI 3.0.0 specification generator with deep  
@@ -33,24 +35,11 @@ You are an expert OpenAPI 3.0.0 specification generator with deep
 - The endpoint path must be unique across the entire specification.  
 - Use the "parameters" attribute to list these details.  
 `
-return rawData
-}
+	guardedData = memguard.NewBufferFromBytes([]byte(rawData))
+	defer guardedData.Destroy()
 
-
-func main() {
-	// Important: Ensure memguard cleans up on interrupt or panic
-	memguard.CatchInterrupt()
-	defer memguard.Purge()
-
-
-	// Create a protected LockedBuffer
-	protectedData = memguard.NewBufferFromBytes([]byte(getData()))
-
-	// Set up HTTP handler
 	http.HandleFunc("/data", dataHandler)
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
 }
