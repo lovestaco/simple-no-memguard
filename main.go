aa023dc21abc932769b02534660e39c679f744cd
lovestaco <athreyac4@gmail.com>
2025-08-03 15:47:35 +0530

    plain code

--- main.go
+++ main.go
@@ -2,27 +2,10 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/awnumar/memguard"
 )
 
-var guardedData *memguard.LockedBuffer
-
-func getData() *memguard.LockedBuffer {
-	return guardedData
-}
-
-func dataHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	dataBuffer := getData()
-	w.Write(dataBuffer.Bytes())
-}
-
-func main() {
-	memguard.CatchInterrupt()
-	defer memguard.Purge()
-
-	rawData := `
+func getData() string {
+	return `
  # Role and Context  
 You are an expert OpenAPI 3.0.0 specification generator with deep  
 
@@ -35,9 +18,14 @@ You are an expert OpenAPI 3.0.0 specification generator with deep
 - The endpoint path must be unique across the entire specification.  
 - Use the "parameters" attribute to list these details.  
 `
-	guardedData = memguard.NewBufferFromBytes([]byte(rawData))
-	defer guardedData.Destroy()
+}
 
+func dataHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(getData()))
+}
+
+func main() {
 	http.HandleFunc("/data", dataHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
